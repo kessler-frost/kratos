c7b91fe4dc53d57e4448913b4577137d718c054d
styx <sanandsankalp@gmail.com>
2025-07-10 02:16:20 -0400

    better

--- kratos/bootstrap_script.py
+++ kratos/bootstrap_script.py
@@ -1,17 +1,15 @@
-# from sh import curl, sh  # pyright: ignore
+from sh import curl, sh  # pyright: ignore
 
-# # Install uv and ollama
-# try:
-#     from sh import uv, ollama  # pyright: ignore
-# except ImportError:
-#     sh(_in=curl("-LsSf", "https://astral.sh/uv/install.sh"))
-#     sh(_in=curl("-LsSf", "https://ollama.com/install.sh"))
+# Install uv and ollama
+try:
+    from sh import uv, ollama  # pyright: ignore
+except ImportError:
+    sh(_in=curl("-LsSf", "https://astral.sh/uv/install.sh"))
+    sh(_in=curl("-LsSf", "https://ollama.com/install.sh"))
 
-# from sh import uv, ollama  # pyright: ignore
+from sh import uv, ollama  # pyright: ignore
 
-# uv.init()
-# uv.add("ollama", "agno")
+uv.init()
+uv.add("ollama", "agno", "cloudpickle")
 
-# print("Installed uv and ollama")
-
-print("Hello, World!")
+print("Installed uv and ollama")
--- kratos/sandbox.py
+++ kratos/sandbox.py
@@ -1,26 +1,17 @@
 from microsandbox import PythonSandbox
-from sh import msb, msx  # pyright: ignore
+from sh import msb  # pyright: ignore
+import random
+import string
+
+# generate a random string of 4 characters
+def generate_random_string(length: int):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
-async def bootstrap_vm(name: str):
+async def bootstrap_vm(prefix: str):
     msb.pull("microsandbox/python")
 
-    async with PythonSandbox.create(name=name) as sb:
+    async with PythonSandbox.create(name=f"{prefix}-{generate_random_string(4)}") as sb:
         with open("bootstrap_script.py", "r") as f:
             exec = await sb.run(f.read())
         print(await exec.output())
-    
-import asyncio
-
-asyncio.run(bootstrap_vm("my-sandbox"))
-
-# def bootstrap_vm(name: str):
-#     msb.pull("microsandbox/python")
-#     with open("bootstrap_script.py", "r") as f:
-#         text = f.read()
-#         # text = "print('Hello, World!')"
-#     print(f"Executing: {text}")
-#     result = msx("python", "--", "-c", text)
-#     print(f"Result: {result}")
-
-# bootstrap_vm("my-sandbox")
